c1e199aa28b7b7d6361b847f628ba5c6a6569102
krishsb2405 <krishsb2405@gmail.com>
2016-10-16 21:08:07 -0700

    final quiz2 with BFS :- depth - 5  and Dijkstra's depth - 5

--- src/main/java/csula/cs4660/quizes/App.java
+++ src/main/java/csula/cs4660/quizes/App.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         // to get a state, you can simply call `Client.getState with the id`
-    	//BFS();
+    	BFS();
     	Dijkstra();
     }
 
@@ -15,22 +15,24 @@ public class App {
 		System.out.println();
 		System.out.println("Dijkstra's path:");
 		State initialState = Client.getState("10a5461773e8fd60940a56d2e9ef7bf4").get();
-		HashMap<State,Integer> distanceOfEachState = new HashMap<State,Integer>();
+		HashMap<String,Integer> distanceOfEachState = new HashMap<String,Integer>();
     	List<State> openStates = new ArrayList<State>();
     	List<State> closedStates = new ArrayList<State>();
     	HashMap<State, State> parents = new HashMap<State,State>();
     	initialState = Client.getState("10a5461773e8fd60940a56d2e9ef7bf4").get();
     	openStates.add(initialState);
-    	distanceOfEachState.put(initialState, 0);
+    	distanceOfEachState.put(initialState.getId(), 0);
     	while(!openStates.isEmpty()){
-    		State evaluationState = getStateWithLowestDistance(distanceOfEachState,openStates);
+    		State evaluationState = getStateWithHighestEffectValue(distanceOfEachState,openStates);
     		openStates.remove(evaluationState);
     		closedStates.add(evaluationState);
     		
     		if(evaluationState.getId().equals("e577aa79473673f6158cc73e0e5dc122")){
     			System.out.println("found solution with depth of "+findDepth(parents, evaluationState, initialState));
+    			break;
     		}
-    		for(State neighbor:Client.getState(evaluationState.getId()).get().getNeighbors()){
+    		State[] neighbors = Client.getState(evaluationState.getId()).get().getNeighbors();
+    		for(State neighbor:neighbors){    			
     			boolean found = false;
         		for(State closedState : closedStates){
         			if(neighbor.equals(closedState)){
@@ -40,15 +42,15 @@ public class App {
     			if(!found){
     				Optional<DTO> dto = Client.stateTransition(neighbor.getId(),evaluationState.getId());
     				int effect = dto.get().getEvent().getEffect();
-    				int newEffect = distanceOfEachState.get(evaluationState) + effect;
-    				if(distanceOfEachState.containsKey(neighbor)){
-    					if(distanceOfEachState.get(neighbor)>newEffect){
-    						distanceOfEachState.put(neighbor, newEffect);
+    				int totalEffectOfEachState = distanceOfEachState.get(evaluationState.getId()) + effect;
+    				if(distanceOfEachState.containsKey(neighbor.getId())){
+    					if(distanceOfEachState.get(neighbor.getId())<totalEffectOfEachState){
+    						distanceOfEachState.put(neighbor.getId(), totalEffectOfEachState);
     						parents.put(neighbor, evaluationState);
     						openStates.add(neighbor);
     					}
     				}else{
-    					distanceOfEachState.put(neighbor, newEffect);
+    					distanceOfEachState.put(neighbor.getId(), totalEffectOfEachState);
     					parents.put(neighbor, evaluationState);
     					openStates.add(neighbor);
     				}
@@ -65,17 +67,20 @@ public class App {
         Map<State, State> parents = new HashMap<>();
         frontier.add(initialState);
         System.out.println("BFS Path:");
-        while (!frontier.isEmpty()) {
+        boolean reachedGoal = false;
+        while (!frontier.isEmpty() && !reachedGoal) {
             State current = frontier.poll();
             exploredSet.add(current);
-            if (current.getId().equals("e577aa79473673f6158cc73e0e5dc122")) {
-                // construct actions from endTile
-                System.out.println("found solution with depth of " + findDepth(parents, current, initialState));
-                break;
-            }
+            
             // for every possible action
             for (State neighbor: Client.getState(current.getId()).get().getNeighbors()) {
                 // state transition
+            	if (neighbor.getId().equals("e577aa79473673f6158cc73e0e5dc122")) {
+                    // construct actions from endTile
+                    System.out.println("found solution with depth of " + findDepth(parents, current, initialState));
+                    reachedGoal = true;
+                    break;
+                }
                 if (!exploredSet.contains(neighbor)) {
                     parents.put(neighbor, current);
                     frontier.add(neighbor);
@@ -93,34 +98,36 @@ public class App {
             path.add(c);
             c = parents.get(c);
         }
+        path.add(start);
         Collections.reverse(path);
         for(int i =0;i < path.size()-1;i++){
         	State firstState = path.get(i);
         	State secondState = path.get(i+1);
         	Optional<DTO> dto = Client.stateTransition(firstState.getId(), secondState.getId());
-        	System.out.println(firstState.getLocation().getName()+":"+secondState.getLocation().getName()+":-"+dto.get().getEvent().getEffect()+"                    ---"+dto.get().getEvent().getDescription());
+        	System.out.println(firstState.getLocation().getName()+":"+secondState.getLocation().getName()+":"+dto.get().getEvent().getEffect());
         }
         return depth;
     }
     
-    private static State getStateWithLowestDistance(HashMap<State, Integer> distanceOfEachState,
+    private static State getStateWithHighestEffectValue(HashMap<String, Integer> distanceOfEachState,
 			List<State> openStates) {
 		// TODO Auto-generated method stub
-    	int min = distanceOfEachState.get(openStates.get(0));
-    	int temp = min;
-    	State shortestState = null;
+    	int max = distanceOfEachState.get(openStates.get(0).getId());
+    	int temp = max;
+    	State highestEffectState = null;
     	for(State n : openStates){
-    		if(distanceOfEachState.containsKey(n)){
-    			if(distanceOfEachState.get(n)>min){
-    				min = distanceOfEachState.get(n);
-    				shortestState = n;
+    		if(distanceOfEachState.containsKey(n.getId())){
+    			int effect = distanceOfEachState.get(n.getId());
+    			if(effect>=max){
+    				max = distanceOfEachState.get(n.getId());
+    				highestEffectState = n;
     			}
     		}
     	}
-    	if(temp==min){
-    		shortestState = openStates.get(0);
+    	if(temp==max){
+    		highestEffectState = openStates.get(0);
     	}
-    	return shortestState;
+    	return highestEffectState;
 		
 	}
 }
