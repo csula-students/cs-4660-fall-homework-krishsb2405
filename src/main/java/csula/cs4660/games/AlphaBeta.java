e1dfb032f4a449a70c41f7f4a160f154de346a96
krishsb2405 <krishsb2405@gmail.com>
2016-11-18 14:09:18 -0800

    MiniMax and Alpha Beta Pruning

--- src/main/java/csula/cs4660/games/AlphaBeta.java
+++ src/main/java/csula/cs4660/games/AlphaBeta.java
@@ -1,11 +1,49 @@
 package csula.cs4660.games;
 
+import csula.cs4660.games.models.MiniMaxState;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
 public class AlphaBeta {
-    public static Node getBestMove(Graph graph, Node source, Integer depth, Integer alpha, Integer beta, Boolean max) {
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public static Node getBestMove(Graph graph, Node<MiniMaxState> source, Integer depth, Integer alpha, Integer beta, Boolean max) {
         // TODO: implement your alpha beta pruning algorithm here
-        return null;
+    	Node<MiniMaxState> v;
+    	if(depth ==0)
+    		return source;
+    	if(max){
+    		v = new Node<>(new MiniMaxState(Integer.MIN_VALUE,Integer.MIN_VALUE));
+    		for(Node<MiniMaxState> eachNeighbor:graph.neighbors(source)){
+    			Node<MiniMaxState> v1 = getBestMove(graph, eachNeighbor, depth-1, alpha, beta, false);
+    			if(v1.getData().getValue()>v.getData().getValue()){
+    				v = v1;
+    			}
+    			Node<MiniMaxState> parent = graph.getNode(source).get();
+    			parent.getData().setValue(v.getData().getValue());
+    			if(v.getData().getValue()>=beta){
+    				
+    				return v;
+    			}
+    			alpha = Math.max(alpha,v.getData().getValue());
+    		}
+    		return v;
+    	}
+    	else{
+    		v = new Node(new MiniMaxState(Integer.MAX_VALUE,Integer.MAX_VALUE));
+    		for(Node eachNeighbor:graph.neighbors(source)){
+    			Node<MiniMaxState> v1 = getBestMove(graph, eachNeighbor, depth-1, alpha, beta, true);
+    			if(v1.getData().getValue()<v.getData().getValue()){
+    				v = v1;
+    			}
+    			Node<MiniMaxState> parent = graph.getNode(source).get();
+    			parent.getData().setValue(v.getData().getValue());
+    			if(v.getData().getValue()<=alpha){
+    					return v;
+    			}
+    			beta = Math.min(beta, v.getData().getValue());
+    		}
+    		return v;
+    	}
+        
     }
 }
--- src/main/java/csula/cs4660/games/MiniMax.java
+++ src/main/java/csula/cs4660/games/MiniMax.java
@@ -1,12 +1,42 @@
 package csula.cs4660.games;
 
+import csula.cs4660.games.models.MiniMaxState;
+
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
 public class MiniMax {
-    public static Node getBestMove(Graph graph, Node root, Integer depth, Boolean max) {
+	@SuppressWarnings("unchecked")
+    public static Node<MiniMaxState> getBestMove(Graph graph, Node<MiniMaxState> root, Integer depth, Boolean max) {
         // TODO: implement minimax to retrieve best move
         // NOTE: you should mutate graph and node as you traverse and update value
-        return null;
+    	Node<MiniMaxState> bestValue;
+    	if(depth==0){
+    		return root;
+    	}
+    	if(max){
+    		bestValue = new Node<>(new MiniMaxState(Integer.MIN_VALUE,Integer.MIN_VALUE)); 
+    		for(Node<MiniMaxState> move:graph.neighbors(root)){
+				Node<MiniMaxState> value = getBestMove(graph, move, depth-1, false);
+    			if(value.getData().getValue()>bestValue.getData().getValue()){
+    				bestValue = value;
+    				Node<MiniMaxState> parent = graph.getNode(root).get();
+    				parent.getData().setValue(bestValue.getData().getValue());
+    			}
+    		}
+    		return bestValue;
+    	}
+    	else{
+    		bestValue = new Node<>(new MiniMaxState(Integer.MAX_VALUE,Integer.MAX_VALUE));
+    		for(Node<MiniMaxState> move:graph.neighbors(root)){
+    			Node<MiniMaxState> value = getBestMove(graph, move, depth-1, true);
+    			if(value.getData().getValue()<bestValue.getData().getValue()){
+    				bestValue = value;
+    				Node<MiniMaxState> parent = graph.getNode(root).get();
+    				parent.getData().setValue(bestValue.getData().getValue());
+    			}
+    		}
+    		return bestValue;
+    	}
     }
 }
